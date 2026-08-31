553ba25c1935322a6d27dc07d68d896469b66811
HieuDao1811 <daohuuhieu18112004@gmail.com>
2026-08-31 21:13:04 +0700

    WIP: temporary save

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/login";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { authService } from "../main";
+import { authService } from "../../main";
 import toast from "react-hot-toast";
 import { useGoogleLogin } from "@react-oauth/google";
 import { FcGoogle } from "react-icons/fc";
